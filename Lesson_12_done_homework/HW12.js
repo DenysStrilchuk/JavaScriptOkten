abf1a38126d10a9a5514a849c20d7579bdeb71a1
Denys Sttrilchuk <denia876@gmail.com>
2023-12-31 15:17:35 +0200

    practise events

--- Lesson_12_done_homework/HW12.js
+++ Lesson_12_done_homework/HW12.js
@@ -4,28 +4,56 @@
 
 
 
-fetch('http://jsonplaceholder.typicode.com/users ')
-    .then(value => value.json())
-    .then(value => {
-        console.log(value);
-        for (const valueElement of value) {
-            let userId = valueElement.id;
-            let userName = valueElement.name;
-            let ul = document.createElement('ul');
-            let li = document.createElement('li');
-            let div = document.createElement('div');
-            let link = document.createElement('a');
-            link.href = `user-details.html?id=${userId}`;
-            link.innerText = `user-details.html?id=${userId}`;
-            li.innerText = `User id: ${userId}; User name: ${userName}; User link: `;
-            li.appendChild(link);
-            ul.appendChild(li);
-            div.appendChild(ul);
-            document.body.appendChild(div);
+// fetch('http://jsonplaceholder.typicode.com/users ')
+//     .then(value => value.json())
+//     .then(value => {
+//         console.log(value);
+//         for (const valueElement of value) {
+//             let userId = valueElement.id;
+//             let userName = valueElement.name;
+//             let ul = document.createElement('ul');
+//             let li = document.createElement('li');
+//             let div = document.createElement('div');
+//             let link = document.createElement('a');
+//             link.href = `user-details.html?id=${userId}`;
+//             link.innerText = `user-details.html?id=${userId}`;
+//             li.innerText = `User id: ${userId}; User name: ${userName}; User link: `;
+//             li.appendChild(link);
+//             ul.appendChild(li);
+//             div.appendChild(ul);
+//             document.body.appendChild(div);
+//         }
+//     })
 
-        }
-    })
+// fetch('http://jsonplaceholder.typicode.com/users ')
+//     .then(value => value.json())
+//     .then(value => {
+//         console.log(value);
+//         for (const valueElement of value) {
+//             let userInfo = document.getElementById('userInfo');
+//             userInfo.innerText = `${valueElement}`;
+//             document.body.appendChild(userInfo);
+//         }
+//     })
 
+// fetch('http://jsonplaceholder.typicode.com/users ')
+//     .then(response => response.json())
+//     .then(users => {
+//         console.log(users);
+//
+//         let userInfo = document.getElementById('userInfo');
+//         for (const user of users) {
+//             for (const key in user) {
+//                 let p = document.createElement('p');
+//                 p.innerText = `${key}: ${user[key]}`;
+//                 userInfo.appendChild(p);
+//             }
+//             userInfo.appendChild(document.createElement('hr')); // Додаємо лінію між користувачами
+//         }
+//
+//         document.body.appendChild(userInfo);
+//     })
+//     .catch(error => console.error('Помилка отримання даних:', error));
 
 
 
