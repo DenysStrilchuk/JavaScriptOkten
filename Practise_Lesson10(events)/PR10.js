ac2ed72f1af101cb8d8e35b500281d1b1efee868
Denys Sttrilchuk <denia876@gmail.com>
2024-01-03 02:05:11 +0200

    practise events Lesson 10

--- Practise_Lesson10(events)/PR10.js
+++ Practise_Lesson10(events)/PR10.js
@@ -270,50 +270,76 @@
 //     }
 // };
 
-let productsDiv = document.getElementsByClassName('products')[0];
+// let productsDiv = document.getElementsByClassName('products')[0];
+//
+//
+// let products = [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+//
+// for (const product of products) {
+//     let productBlock = document.createElement('div');
+//     productsDiv.appendChild(productBlock);
+//     let h2 = document.createElement('h2');
+//     h2.innerText = `${product.title} ${product.price} UAH`;
+//     let img = document.createElement('img');
+//     img.src = product.image;
+//     img.classList.add('imgClass');
+//     let button = document.createElement('button');
+//     button.innerText = 'add to cart';
+//     productBlock.append(h2,img,button);
+//
+//     button.onclick = function(){
+//         let cart = JSON.parse(localStorage.getItem('cart')) || [];
+//         cart.push(product);
+//         localStorage.setItem('cart', JSON.stringify(cart));
+//     }
+// }
 
 
-let products = [
-    {
-        title: 'milk',
-        price: 22,
-        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
-    },
-    {
-        title: 'juice',
-        price: 27,
-        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-    },
-    {
-        title: 'tomato',
-        price: 47,
-        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-    },
-    {
-        title: 'tea',
-        price: 15,
-        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-    },
-];
+// Стоврити форму з трьома полями для name,surname,age та кнопкою. При натисканні на кнопку зчитати данні з полів,
+// та вивести об'єкт в документ. Іншими словами : заповниои форму, натиснули кнопку, під формою з'явився блок з вашим
+// об'єктом
 
-for (const product of products) {
-    let productBlock = document.createElement('div');
-    productsDiv.appendChild(productBlock);
-    let h2 = document.createElement('h2');
-    h2.innerText = `${product.title} ${product.price} UAH`;
-    let img = document.createElement('img');
-    img.src = product.image;
-    img.classList.add('imgClass');
-    let button = document.createElement('button');
-    button.innerText = 'add to cart';
-    productBlock.append(h2,img,button);
-
-    button.onclick = function(){
-        let cart = JSON.parse(localStorage.getItem('cart')) || [];
-        cart.push(product);
-        localStorage.setItem('cart', JSON.stringify(cart));
+let usersArr = [];
+let form = document.forms['f1'];
+form.onsubmit = function(e) {
+    e.preventDefault();
+    let name = this.name.value;
+    let surname = this.surname.value;
+    let age = this.age.value;
+    console.log({name,surname,age});
+    usersArr.push({name, surname, age});
+}
+let btn = document.getElementById('btn-show');
+btn.onclick = function (){
+    for (const user of usersArr) {
+        let div = document.createElement('div');
+        div.innerText = `${user.name} ${user.surname} ${user.age}`;
+        document.body.appendChild(div);
     }
 }
+
+
 
 
 
