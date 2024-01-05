6f44ff9d92e684780a142cb30269f8a1c63edbbe
Denys Sttrilchuk <denia876@gmail.com>
2024-01-05 23:28:23 +0200

    practise

--- Practise/PR12/main.js
+++ Practise/PR12/main.js
@@ -22,22 +22,25 @@
 //     }
 // })
 
-let url = new URL('https://rickandmortyapi.com/api/character');
+//let url = new URL('https://rickandmortyapi.com/api/character');
 //let url = new URL('https://jsonplaceholder.typicode.com/users');
-//let url = new URL('https://dummyjson.com/products');
+let url = new URL('https://dummyjson.com/products');
 
 let div = document.createElement('div');
 div.classList.add('design');
 document.body.appendChild(div);
 
-fetch(url).then(response => response.json()).then(({results:items}) => {
+//fetch(url).then(response => response.json()).then(({results:items})
+//fetch(url).then(response => response.json()).then((items)=> {
+fetch(url).then(response => response.json()).then(({products:items})=> {
     console.log(items);
     items.forEach(item => {
         console.log(item);
         let p = document.createElement('p');
         let btn = document.createElement('button');
         btn.classList.add('btnStyle');
-        p.innerText = `${item.id} - ${item.name}`;
+        //p.innerText = `${item.id} - ${item.name}`;
+        p.innerText = `${item.id} - ${item.title}`;
         btn.innerText = 'DETAILS';
         div.append(p,btn);
         btn.addEventListener('click',() => {
